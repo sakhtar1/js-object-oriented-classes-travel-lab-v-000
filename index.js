ebc38f295ad1d1f91de687aebc45d6cf516d8bb1
Sharmina Akhtar <sakht318@gmail.com>
2018-08-06 14:20:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 class Driver {
-  constructor(name, date) {
+  constructor(name, startDate) {
   this.name = name
-  this.date = date
+  this.startDate = new Date(startDate)
   }
+
+  yearsExperienceFromBeginningOf() {
+    const date = new Date()
+    date.setYear(endDate)
+    return Math.abs(this.startDate.getFullYear() - date.getFullYear());
+  }
+}
 
 
   class Route {
