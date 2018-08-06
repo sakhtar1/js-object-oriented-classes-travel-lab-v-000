41b7435acdb294dee3d5eccfb69c2ab86b19227e
Sharmina Akhtar <sakht318@gmail.com>
2018-08-06 14:25:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,4 +17,18 @@ class Driver {
     this.beginningLocation = beginningLocation
     this.endingLocation = endingLocation
     }
-}
+
+    blocksTravelled() {
+    let eastWest =  ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
+  }
+  const distA = Math.abs(eastWest.indexOf(this.beginningLocation.horizontal) - eastWest.indexOf(this.endingLocation.horizontal));
+    const distB = Math.abs(this.beginningLocation.vertical - this.endingLocation.vertical)
+    return distA + distB;
+  }
+   estimatedTime(peak) {
+    if (peak){
+      return this.blocksTravelled()/2
+    } else {
+      return this.blocksTravelled()/3
+    }
+  }
