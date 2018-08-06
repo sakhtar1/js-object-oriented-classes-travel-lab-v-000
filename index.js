134c68ea67653bacc0f8d219cb0e8419c26853ca
Sharmina Akhtar <sakht318@gmail.com>
2018-08-06 15:01:47 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
+class Driver {
+  constructor(name, startDate) {
+  this.name = name
+  this.startDate = new Date(startDate)
+  }
 
+  yearsExperienceFromBeginningOf(year) {
+    const date = new Date(year, 1, 1)
+    return date.getFullYear() - this.startDate.getFullYear()-1;
+  }
+
+
+}
+
+
+  class Route {
+    constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation
+    this.endingLocation = endingLocation
+    }
+
+    blocksTravelled() {
+    let eastWest =  ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
+
+    const distA = Math.abs(eastWest.indexOf(this.endingLocation.horizontal) - eastWest.indexOf(this.beginningLocation.horizontal));
+    const distB = Math.abs(this.endingLocation.vertical - this.beginningLocation.vertical)
+    return distB + distA;
+  }
+   estimatedTime(peak) {
+    if (peak){
+      return this.blocksTravelled()/2
+    } else {
+      return this.blocksTravelled()/3
+    }
+  }
+}
