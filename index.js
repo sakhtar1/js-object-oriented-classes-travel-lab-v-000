b19a176ea31a7583a620433f068206c8a61aaa64
Sharmina Akhtar <sakht318@gmail.com>
2018-08-06 14:17:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+class Driver {
+  constructor(name, date) {
+  this.name = name
+  this.date = date
+  }
 
+
+  class Route {
+    constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation
+    this.endingLocation = endingLocation
+    }
